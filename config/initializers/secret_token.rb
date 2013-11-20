27523988c42b650a376602a275ebdcdbb104ace8
James Davis <jamesmdavis5@gmail.com>
2013-11-20 16:40:18 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BusApp::Application.config.secret_token = 'a687689c77d673abbed12830dd2663ed66844c91cca4465272b2b75659a16efe6b9d1ef7cb4e8cc8d9ef36111f3401c6bb8765d16e4df7f421ca8411b85937ad'
